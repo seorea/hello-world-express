15ef2827dcad1c9a5e8f3741dc9d59d1db0bb94d
Seoyaa <kevinyang5694@gmail.com>
2019-02-21 13:49:22 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.get('/', function(req, res)
+{
+  res.send('Hello World');
+});
+
+app.listen(3000, function()
+{
+  console.log('Server started on Port 3000...');
+})
